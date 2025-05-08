ab0254845ac6706a38d37984e2d1d2486323bc26
rmgal <renov.gorbachev@gmail.com>
2025-05-07 22:21:02 -0700

    stripe require

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const Stripe = require("stripe");
 require("dotenv").config();
 
 const Product = require("./models/Product");
