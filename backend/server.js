1647d27cb0057899585b2ef4944a5833ca6f9d20
rmgal <renov.gorbachev@gmail.com>
2025-05-22 21:02:44 -0700

    product details be

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,14 @@ app.get("/api/products", async (req, res) => {
     res.status(500).json({ message: "Server Error" });
   }
 });
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.json(product);
+  } catch (err) {
+    res.status(404).json({ message: "Product not found" });
+  }
+});
 
 app.listen(5000, () => console.log("Server running on port 5000"));
 
